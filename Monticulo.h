5dd6b1b22c968dcdc5427701a027dc8bf3a3de23
Javier Felipe Aldana Jaramillo <al_javier@javeriana.edu.co>
2025-03-31 22:09:58 +0000

    America de Cali

--- Monticulo.h
+++ Monticulo.h
@@ -1,32 +1,26 @@
 #ifndef MONTICULO_H
 #define MONTICULO_H
 
-#include <vector>
+#include <deque>
 #include <algorithm>
 #include <functional>
-#include <unordered_set> // Agregado para mejorar eficiencia en búsqueda
+#include <list>
 
-template<class T, class Compare = std::greater<T>>
+template<class T>
 class Monticulo {
 private:
-    std::vector<T> datos;
-    Compare comparador;
-    std::unordered_set<T> elementos; // Para evitar duplicados en O(1)
-
-    void heapifyUp(size_t indice);
-    void heapifyDown(size_t indice);
+    std::deque<T> datos;
+    std::greater<T> comparador; // Cambiado a comparador fijo
     
 public:
-    Monticulo() = default;
-
-    void insertar(const T& valor);
-    bool eliminar(const T& valor);
-    bool buscar(const T& valor) const;
-    void inordenEnLista(std::list<T>& lista) const;
-    bool vacio() const { return datos.empty(); }
-    size_t tamano() const { return datos.size(); }
+    void insertar(T valor);
+    bool eliminar(T valor);
+    bool buscar(T valor);
+    void inordenEnLista(std::list<T>& lista);
+    bool vacio();
+    size_t tamano();
 };
 
 #include "Monticulo.hxx"
 
-#endif // MONTICULO_H
+#endif // MONTICULO_H
--- Monticulo.hxx
+++ Monticulo.hxx
@@ -1,79 +1,48 @@
 #include "Monticulo.h"
 
-template<class T, class Compare>
-void Monticulo<T, Compare>::heapifyUp(size_t indice) {
-    while (indice > 0) {
-        size_t padre = (indice - 1) / 2;
-        if (comparador(datos[padre], datos[indice])) {
-            std::swap(datos[padre], datos[indice]);
-            indice = padre;
-        } else {
-            break;
-        }
+template<class T>
+void Monticulo<T>::insertar(T valor) {
+    // Verificar si ya existe (no permitir duplicados)
+    if (std::find(datos.begin(), datos.end(), valor) != datos.end()) {
+        return;
     }
-}
-
-template<class T, class Compare>
-void Monticulo<T, Compare>::heapifyDown(size_t indice) {
-    size_t hijoIzq, hijoDer, hijoMayor;
-    size_t n = datos.size();
     
-    while (true) {
-        hijoIzq = 2 * indice + 1;
-        hijoDer = 2 * indice + 2;
-        hijoMayor = indice;
-        
-        if (hijoIzq < n && comparador(datos[hijoIzq], datos[hijoMayor])) {
-            hijoMayor = hijoIzq;
-        }
-        
-        if (hijoDer < n && comparador(datos[hijoDer], datos[hijoMayor])) {
-            hijoMayor = hijoDer;
-        }
-        
-        if (hijoMayor != indice) {
-            std::swap(datos[indice], datos[hijoMayor]);
-            indice = hijoMayor;
-        } else {
-            break;
-        }
-    }
+    datos.push_back(valor);
+    std::push_heap(datos.begin(), datos.end(), comparador);
 }
 
-template<class T, class Compare>
-void Monticulo<T, Compare>::insertar(const T& valor) {
-    if (elementos.insert(valor).second) { 
-        datos.push_back(valor);
-        heapifyUp(datos.size() - 1);
-    }
-}
-
-template<class T, class Compare>
-bool Monticulo<T, Compare>::eliminar(const T& valor) {
+template<class T>
+bool Monticulo<T>::eliminar(T valor) {
     auto it = std::find(datos.begin(), datos.end(), valor);
     if (it == datos.end()) return false;
     
-    *it = datos.back();
+    // Mover el elemento al final
+    std::swap(*it, datos.back());
     datos.pop_back();
-    elementos.erase(valor); // También eliminar del set
-
-    if (!datos.empty()) {
-        size_t indice = it - datos.begin();
-        heapifyDown(indice);
-        heapifyUp(indice);
-    }
     
+    // Rehacer el heap
+    std::make_heap(datos.begin(), datos.end(), comparador);
     return true;
 }
 
-template<class T, class Compare>
-bool Monticulo<T, Compare>::buscar(const T& valor) const {
-    return elementos.count(valor) > 0; // O(1) en promedio
+template<class T>
+bool Monticulo<T>::buscar(T valor) {
+    return std::find(datos.begin(), datos.end(), valor) != datos.end();
 }
 
-template<class T, class Compare>
-void Monticulo<T, Compare>::inordenEnLista(std::list<T>& lista) const {
-    std::vector<T> copia = datos;
-    std::sort(copia.begin(), copia.end());
+template<class T>
+void Monticulo<T>::inordenEnLista(std::list<T>& lista) {
+    std::deque<T> copia(datos.begin(), datos.end());
+    std::sort(copia.begin(), copia.end(), std::less<T>());
     lista.assign(copia.begin(), copia.end());
+}
+
+template<class T>
+bool Monticulo<T>::vacio() {
+    return datos.empty();
+}
+
+template<class T>
+size_t Monticulo<T>::tamano() {
+    return datos.size();
 }
