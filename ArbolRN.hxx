807d17e44ef408c739c4f40ef106ac9ff457feba
JuanesDT027 <juanesdiaztoledo@gmail.com>
2025-03-31 18:35:24 -0500

    Update ArbolRN.hxx

--- ArbolRN.hxx
+++ ArbolRN.hxx
@@ -8,22 +8,22 @@ template <typename T>
 ArbolRN<T>::ArbolRN() {}
 
 template <typename T>
-void ArbolRN<T>::insertar(const T& value) {
+void ArbolRN<T>::insertar(T value) {
     tree.insert(value);
 }
 
 template <typename T>
-void ArbolRN<T>::eliminar(const T& value) {
+void ArbolRN<T>::eliminar(T value) {
     tree.erase(value);
 }
 
 template <typename T>
-bool ArbolRN<T>::buscar(const T& value) const {
+bool ArbolRN<T>::buscar(T value) {
     return tree.find(value) != tree.end();
 }
 
 template <typename T>
-void ArbolRN<T>::inordenEnLista(std::list<T>& lista) const {
+void ArbolRN<T>::inordenEnLista(std::list<T>& lista) {
     lista.assign(tree.begin(), tree.end());
 }
 
